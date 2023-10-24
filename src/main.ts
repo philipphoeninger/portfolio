4d7bd515b41e40b8bb0e1be9b044479378799333
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2023-10-24 23:51:06 +0200

    remove unnecessary import

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { PhMenu } from "./app/components/ph-menu/ph-menu.component";
 import { PhSelectInfo } from "./app/components/ph-select-info/ph-select-info.component";
 import { PhSelectLocale } from "./app/components/ph-select-locale/ph-select-locale.component";
 import { PhTooltip } from "./app/components/ph-tooltip/ph-tooltip.component";
-import "@material/mwc-circular-progress";
 
 export const components = [
   Page,
