3f53f470236ec075a4bf361c1b4789a13f8be65a
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2023-10-26 23:50:04 +0200

    generate & include translation files

---
+++ src/assets/i18n/locale-codes.ts
@@ -0,0 +1,35 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize.
+
+/**
+ * The locale code that templates in this source code are written in.
+ */
+export const sourceLocale = `en`;
+
+/**
+ * The other locale codes that this application is localized into. Sorted
+ * lexicographically.
+ */
+export const targetLocales = [
+  `de-DE`,
+  `es-ES`,
+  `fr-FR`,
+  `it-IT`,
+  `ja-JP`,
+  `tr-TR`,
+  `zh-Hans`,
+] as const;
+
+/**
+ * All valid project locale codes. Sorted lexicographically.
+ */
+export const allLocales = [
+  `de-DE`,
+  `en`,
+  `es-ES`,
+  `fr-FR`,
+  `it-IT`,
+  `ja-JP`,
+  `tr-TR`,
+  `zh-Hans`,
+] as const;
