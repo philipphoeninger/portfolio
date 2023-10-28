747de3d33196cf7921bb5a31c7063abeabdb0ab4
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2023-10-28 02:11:57 +0200

    add logo icons & update data

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -5,7 +5,7 @@ export const data = {
     {
       area: EnSkillArea.frontend,
       label: "HTML",
-      iconName: "",
+      iconName: "html.svg",
       duration: 4,
       usage: "University and all companies",
       description: "Small description of HTML journey.",
@@ -13,7 +13,7 @@ export const data = {
     {
       area: EnSkillArea.frontend,
       label: "CSS",
-      iconName: "",
+      iconName: "css.svg",
       duration: 4,
       usage: "University and all companies",
       description: "Small description of CSS journey.",
@@ -21,7 +21,7 @@ export const data = {
     {
       area: EnSkillArea.frontend,
       label: "JavaScript",
-      iconName: "",
+      iconName: "javascript_2.svg",
       duration: 1,
       usage: "Company #2 and Udemy course",
       description: "Small description of JavaScript journey.",
@@ -29,7 +29,7 @@ export const data = {
     {
       area: EnSkillArea.frontend,
       label: "TypeScript",
-      iconName: "",
+      iconName: "typescript.svg",
       duration: 3,
       usage: "Company #1 and #2",
       description: "Small description of TypeScript journey.",
@@ -37,7 +37,7 @@ export const data = {
     {
       area: EnSkillArea.frontend,
       label: "Angular",
-      iconName: "",
+      iconName: "angular.svg",
       duration: 3,
       usage: "Company #1 and #2",
       description: "Small description of Angular journey.",
@@ -45,15 +45,39 @@ export const data = {
     {
       area: EnSkillArea.frontend,
       label: "Lit",
-      iconName: "",
+      iconName: "lit.svg",
       duration: 0.25,
       usage: "Personal project",
       description: "Small description of Lit journey.",
     },
+    {
+      area: EnSkillArea.backend,
+      label: "C#",
+      iconName: "angular.svg",
+      duration: 0.25,
+      usage: "Company #2",
+      description: "Small description of C# journey.",
+    },
+    {
+      area: EnSkillArea.backend,
+      label: "Asp.Net",
+      iconName: "angular.svg",
+      duration: 0.1,
+      usage: "Company #2",
+      description: "Small description of Asp.Net journey.",
+    },
+    {
+      area: EnSkillArea.backend,
+      label: "Java",
+      iconName: "java.svg",
+      duration: 0.2,
+      usage: "University Bachelors degree",
+      description: "Small description of Java journey.",
+    },
   ],
   work: [
     {
-      imageName: "portrait.jpg",
+      imageName: "statista_img_dummy.png",
       label: "Statista Program",
       description:
         "This is the description for the Statista Program. It was a pretty complex project. Not too long.",
@@ -78,7 +102,7 @@ export const data = {
       liveUrl: "www.google.com",
     },
     {
-      imageName: "portrait.jpg",
+      imageName: "find_my_friends_img_dummy.png",
       label: "UI Project",
       description:
         "The UI Project was a lot of fun with limited complexity and a lot of bubble gum. Not too long.",
