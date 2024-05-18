775ced13eb2641b615515da7794c6417528020e8
Philipp Höninger <44400490+p-hoeni@users.noreply.github.com>
2024-05-18 11:15:13 +0200

    configure non-relative module names

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@assets": "./src/assets",
+      "@models": "./src/app/models",
+    },
+  },
+  assetsInclude: ["./src/assets/img"],
+  build: {
+    outDir: "./dist",
+    assetsDir: "./assets",
+  },
 });
